e0ffdf3aaeb2def0db51110ce95be6fc319312cf
SvitLanaTy <stymchenko@gmail.com>
2024-09-15 23:34:58 +0200

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,8 @@
 import axios from "axios";
-import { Image } from "../types";
+import { Data } from "../types";
 
 axios.defaults.baseURL = "https://api.unsplash.com/";
 const ACCESS_KEY = "HfYBa1KQn2ueT9q36IJXu8vZr-JdVnbbkdLUim2_q3M";
-
-interface ApiResponse {
-  results: Image[];
-  total: number;
-  total_pages: number;
-}
 
 interface Params {
   client_id: string;
@@ -17,11 +11,8 @@ interface Params {
   per_page: number;
 }
 
-export async function getData(
-  query: string,
-  page: number = 1
-): Promise<ApiResponse> {
-  const { data } = await axios.get<ApiResponse>(`/search/photos`, {
+export async function getData(query: string, page: number = 1): Promise<Data> {
+  const { data } = await axios.get<Data>(`/search/photos`, {
     params: <Params>{
       client_id: ACCESS_KEY,
       query,
